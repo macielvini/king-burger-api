53a858a890807040afd4489ad2eecb036e205c00
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-14 15:49:50 -0300

    feat: create user schema validation

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(body) {
+  create(body: CreateUserDTO) {
     return this.prisma.user.create({ data: body });
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepo: UsersRepository) {}
 
-  async create(body) {
+  async create(body: CreateUserDTO) {
     return await this.usersRepo.create(body);
   }
 }
