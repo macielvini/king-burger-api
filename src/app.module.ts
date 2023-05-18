15d45b120b8f0f886afbb6bb1b898396c7bd2a78
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-18 18:45:14 -0300

    fix: missing ItemsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { AuthController } from './auth/auth.controller';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [AppController, AuthController, UsersController],
+  imports: [AuthModule, UsersModule, ItemsModule],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
