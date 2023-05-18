54afac7b445a19d92f2fb36d197d46094b74a983
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-18 20:28:29 -0300

    feat: create module bash

--- nestmodule.sh
+++ nestmodule.sh
@@ -11,10 +11,11 @@ capitalized="$(tr '[:lower:]' '[:upper:]' <<<${name:0:1})${name:1}"
 
 mkfile() { mkdir -p -- "$1" && touch -- "$1"/"$2"; }
 
+# OPTIONAL CREATE MODULE FUNCTION
 function mkmodule() {
   echo "import { Module } from '@nestjs/common';" >>$name/$name.module.ts
   echo "@Module({})" >>$name/$name.module.ts
   echo "export class ${capitalized}Module {}" >>$name/$name.module.ts
 }
 
-cd "src" && mkdir -p -- $name && mkfile $name/controller index.ts && mkfile $name/services index.ts && mkfile $name/repositories index.ts && mkmodule
+cd "src" && mkdir -p -- $name && mkfile $name/controller index.ts && mkfile $name/services index.ts && mkfile $name/repositories index.ts && nest g mo $name --no-spec
